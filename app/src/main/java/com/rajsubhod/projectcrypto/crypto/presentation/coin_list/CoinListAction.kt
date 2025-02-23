239d715020d93d920616fdaece1cda56c8917649
Rajdeep <rajdeepmukherjee2002@gmail.com>
2025-02-23 23:24:27 +0530

    Coin Details Screen

--- app/src/main/java/com/rajsubhod/projectcrypto/crypto/presentation/coin_list/CoinListAction.kt
+++ app/src/main/java/com/rajsubhod/projectcrypto/crypto/presentation/coin_list/CoinListAction.kt
@@ -3,5 +3,5 @@ package com.rajsubhod.projectcrypto.crypto.presentation.coin_list
 import com.rajsubhod.projectcrypto.crypto.presentation.models.CoinUi
 
 sealed interface CoinListAction {
-    data class OnCoinClick(val coinUInt: CoinUi) : CoinListAction
+    data class OnCoinClick(val coinUi: CoinUi) : CoinListAction
 }
